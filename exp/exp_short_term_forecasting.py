7e2e6b4f29ace73707ed280fe6434dc8c24dfde8
TilliRS <pptry@mail.de>
2024-11-13 18:00:55 +0100

    validation function

--- exp/exp_short_term_forecasting.py
+++ exp/exp_short_term_forecasting.py
@@ -119,11 +119,11 @@ class Exp_Short_Term_Forecast(Exp_Basic):
             print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
             train_loss = np.average(train_loss)
             print(f"train loss: {train_loss}")
-            # vali_loss = self.vali(train_loader, vali_loader, criterion)
-            # test_loss = vali_loss
-            # print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
-            #     epoch + 1, train_steps, train_loss, vali_loss, test_loss))
-            # early_stopping(vali_loss, self.model, path)
+            vali_loss = self.vali(train_loader, vali_loader, criterion)
+            test_loss = vali_loss
+            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
+                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+            early_stopping(vali_loss, self.model, path)
             if early_stopping.early_stop:
                 print("Early stopping")
                 break
